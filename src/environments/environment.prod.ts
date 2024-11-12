396b42b515ff28197f4252755192bd2a0e04e855
Christ-Fuchs <christ.fuchs@hotmail.com>
2024-11-12 20:50:12 +0100

    CategoryService hinzugefügt, um HTTP Requests an den Backens-Server senden zu können.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8',
+    apiKey: 'AIzaSyCHkC-9GFvrbXMKLIgua2yAtgCibPLddh8',
     authDomain: 'budget-planner-7eddb.firebaseapp.com',
     projectId: 'budget-planner-7eddb',
-    appId: '1:278151755192:web:227ae9460314c348be4b4d',
-  }
+    appId: '1:278151755192:web:227ae9460314c348be4b4d'
+  },
+  backendUrl: 'https://budget-service.onrender.com'
 };
